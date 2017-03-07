da3c7e1b1a5861e7e0d6f7d1dfdaab85333a84a8
Esdras Portillo <esdrasportillo@hotmail.com>
2017-03-07 10:40:30 +0000

    Update index.js

--- index.js
+++ index.js
@@ -269,7 +269,8 @@ let styles = StyleSheet.create({
 	},
 	up: {
 		height: 90,
-		overflow: 'hidden'
+		overflow: 'hidden',
+		backgroundColor: 'transparent'
 	},
 	upView: {
 		justifyContent: 'flex-start',
@@ -289,6 +290,7 @@ let styles = StyleSheet.create({
 		height: 40,
 		width: width,
 		overflow: 'hidden',
+		backgroundColor: 'transparent',
 		borderColor: '#aaa',
 		borderTopWidth: 1/ratio,
 		borderBottomWidth: 1/ratio
@@ -309,10 +311,12 @@ let styles = StyleSheet.create({
 	},
 	down: {
 		height: 90,
-		overflow: 'hidden'
+		overflow: 'hidden',
+		backgroundColor: 'transparent'
 	},
 	downView: {
 		overflow: 'hidden',
+		backgroundColor: 'transparent',
 		justifyContent: 'flex-start',
 		alignItems: 'center'
 	},
